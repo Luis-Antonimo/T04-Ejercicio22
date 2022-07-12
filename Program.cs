462b28f636dd058e564116ea9bc1327bbc7c348a
Luis Antónimo <luisantonimo@gmail.com>
2022-07-12 09:12:30 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace T04Ejercicio22
+{
+    class Program
+    {
+        static void Main()
+        {
+            string password = "Nomas2020";
+            bool valor = false;
+            int x = 0;
+            while (valor == false && x < 3)
+            {
+                Console.WriteLine("Escriba la contraseña (tiene 3 intentos):");
+                String escrito = Console.ReadLine();
+                valor = Equals(password, escrito);
+                x++;
+            }
+            if (valor)
+            {
+                Console.WriteLine("Enhorabuena.");
+            }
+            else
+            {
+                Console.WriteLine("Ha superado los tres intentos.");
+            }
+        }
+    }
+}
